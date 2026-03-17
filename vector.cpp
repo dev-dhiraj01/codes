3f0407825fcfc9d307dd1466d7a0909dd43d1583
Dhiraj70C <dchaudhari1256@gmail.com>
2026-03-17 08:44:49 +0530

    removed .exe files

--- vector.cpp
+++ vector.cpp
@@ -10,12 +10,30 @@ int main(){
 
     vector<int> distance(15,0);
 
+    int arr[] = {10,20,30,40,50,60};
+    
+    //for each loop to access the elements 
+    for(int i : arr){
+        cout<<i<<endl;
+    }
+
     marks.push_back(10);
     marks.push_back(20);
     marks.push_back(30);
+  
+
+    //traversing using iterator
+    vector<int>::iterator it = marks.begin();
+cout<<"printing using iterator"<<endl;
+while(it != marks.end()){
+    cout<<*it<<endl;
+    it++;
+}
+
+cout<<"printing using for loop "<<endl;
     for(int i = 0; i < 3; i++ ){
-        cout<<*marks.begin()<<endl;
-        marks.begin();
+        cout<<marks[i]<<endl;
+
     }
 return 0;
 }
