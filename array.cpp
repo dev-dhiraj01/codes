c1f094c46e6154751b48c4cab6e5b8030e775ce4
Dhiraj70C <dchaudhari1256@gmail.com>
2026-03-28 21:49:39 +0530

    added code

--- array.cpp
+++ array.cpp
@@ -9,5 +9,9 @@ int main(){
         cout<<arr[i]<<" ";
     }
 
+    cout<<"Element at index 2 is: "<<arr.at(2)<<endl;
+    cout<<"First element is: "<<arr.front()<<endl;
+    cout<<"Last element is: "<<arr.back()<<endl;
+    cout<<"Is the array empty? "<<(arr.empty()?"Yes":"No")<<endl;
 return 0;
 }
