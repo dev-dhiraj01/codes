428c2332796f577c85cfaa2c757fcd292d51d589
Dhiraj70C <dchaudhari1256@gmail.com>
2026-04-01 23:11:09 +0530

    added addition of metrix

--- twoDArray.cpp
+++ twoDArray.cpp
@@ -27,5 +27,25 @@ int main()
         cout << endl;
     }
 
+    int a[2][2] = {{1, 2}, {3, 4}};
+    int b[2][2] = {{5, 6}, {7, 8}};
+    int sum[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    cout << "Sum Matrix:\n";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
